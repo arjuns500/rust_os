becd1dd7cc5930efa061291a629efb56caff5353
Arjun S <arjunsenthilvel1@gmail.com>
2022-03-08 11:50:32 -0600

    Finished vga buffer code Println macros now work

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,18 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
-
-static HELLO: &[u8] = b"Hello World!";
+use lazy_static::lazy_static;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    panic!("Panic message");
     loop {}
 }
 
 #[panic_handler]
 fn panic(_inf: &PanicInfo) -> ! {
+    println!("{}", _inf);
     loop {}
 }
